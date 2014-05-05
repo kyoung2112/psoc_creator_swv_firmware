0cbd0396f5cdc0239dda55fe40314cfc7ef277d1
Kris Young <kyoung2112@gmail.com>
2014-05-05 16:23:45 -0400

    Added license file. Added banner to main.c and cleaned up some commenting.

--- Firmware_SWV_Example.cydsn/main.c
+++ Firmware_SWV_Example.cydsn/main.c
@@ -1,17 +1,42 @@
-/* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
- * This file is necessary for your project to build.
- * Please do not delete it.
- *
- * ========================================
-*/
+/******************************************************************************
+* Project Name		: Firmware SWV Example
+* File Name			: main.c
+* Version 			: 1.0
+* Device Used		: CY8C3866AXI-040, CY8C5858AXI-LP035
+* Software Used		: PSoC Creator 3.0
+* Compiler    		: ARMGCC, Keil 8051
+* Related Hardware	: CY8CKIT-001, CY8CKIT-030, CY8CKIT-050
+*
+********************************************************************************
+* Copyright (2014), Cypress Semiconductor Corporation. All Rights Reserved.
+********************************************************************************
+* This software is owned by Cypress Semiconductor Corporation (Cypress)
+* and is protected by and subject to worldwide patent protection (United
+* States and foreign), United States copyright laws and international treaty
+* provisions. Cypress hereby grants to licensee a personal, non-exclusive,
+* non-transferable license to copy, use, modify, create derivative works of,
+* and compile the Cypress Source Code and derivative works for the sole
+* purpose of creating custom software in support of licensee product to be
+* used only in conjunction with a Cypress integrated circuit as specified in
+* the applicable agreement. Any reproduction, modification, translation,
+* compilation, or representation of this software except as specified above 
+* is prohibited without the express written permission of Cypress.
+*
+* Disclaimer: CYPRESS MAKES NO WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, WITH 
+* REGARD TO THIS MATERIAL, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+* Cypress reserves the right to make changes without further notice to the 
+* materials described herein. Cypress does not assume any liability arising out 
+* of the application or use of any product or circuit described herein. Cypress 
+* does not authorize its products for use as critical components in life-support 
+* systems where a malfunction or failure may reasonably be expected to result in 
+* significant injury to the user. The inclusion of Cypress' product in a life-
+* support systems application implies that the manufacturer assumes all risk of 
+* such use and in doing so indemnifies Cypress against all charges. 
+*
+* Use of this Software may be limited by and subject to the applicable Cypress
+* software license agreement. 
+*******************************************************************************/
 
 #include <device.h>
 #include <stdio.h>
@@ -33,13 +58,11 @@
 #define BAUD_DIVISOR	0x0000
 
 #if !(CY_PSOC3)
-//struct __FILE { int handle; /* Add whatever you need here */ };
-//FILE __stdout;
-//FILE __stdin;
-#define CYREG_SWV_ITM_SPR_DATA0 CYDEV_ITM_BASE
-#define CYREG_SWV_ITM_SPR_DATA1 CYDEV_ITM_BASE+4
-#define CYREG_SWV_ITM_SPR_DATA2 CYDEV_ITM_BASE+8
-#define CYREG_SWV_ITM_SPR_DATA4 CYDEV_ITM_BASE+16
+/* PSoC5LP SWV stimulus register defines */
+	#define CYREG_SWV_ITM_SPR_DATA0 CYDEV_ITM_BASE
+	#define CYREG_SWV_ITM_SPR_DATA1 CYDEV_ITM_BASE+4
+	#define CYREG_SWV_ITM_SPR_DATA2 CYDEV_ITM_BASE+8
+	#define CYREG_SWV_ITM_SPR_DATA4 CYDEV_ITM_BASE+16
 #endif
 
 void CySwvEnable()
